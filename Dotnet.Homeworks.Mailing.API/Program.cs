77edbe2a8f11b865f6b5947c50ed7ff8e53bd3e9
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-11-07 13:22:36 +0300

    Did Hw5

--- Dotnet.Homeworks.Mailing.API/Program.cs
+++ Dotnet.Homeworks.Mailing.API/Program.cs
@@ -1,11 +1,20 @@
 using Dotnet.Homeworks.Mailing.API.Configuration;
 using Dotnet.Homeworks.Mailing.API.Services;
+using Dotnet.Homeworks.Mailing.API.ServicesExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.Configure<EmailConfig>(builder.Configuration.GetSection("EmailConfig"));
 
 builder.Services.AddScoped<IMailingService, MailingService>();
+
+builder.Services.AddMasstransitRabbitMq(new RabbitMqConfig
+{
+    Username = "rabbit",
+    Password = "admin",
+    Hostname = "rabbitmq"
+});
 
 var app = builder.Build();
 
